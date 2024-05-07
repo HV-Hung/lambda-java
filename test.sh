b8ac9acc12db162fa0e3953a69b6914428b7797a
Hoang Van Hung <hoangvanhung6876@gmail.com>
2024-05-07 16:22:01 +0700

    work-dir

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
-functions=$(find functions -maxdepth 1 -mindepth 1 -type d -print | jq -R . | jq -s . )
-echo $functions
+filter=""
+work_dirs="["
+
+for dir in $(find functions -mindepth 1 -maxdepth 1 -type d); do
+  dir=${dir#functions/}
+  
+  filter+="\"$dir\":\"functions/$dir/**\","
+  work_dirs+="\"$dir\","
+done
+
+filter="{${filter%,}}"
+
+work_dirs="${work_dirs%,}]"
+
+echo $filter
+echo $work_dirs
